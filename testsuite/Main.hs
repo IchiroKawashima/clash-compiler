b6baa1f82cbfd57708875154a2524c1c055a4161
Ben Blaxill <ben.blaxill@gmail.com>
2019-11-15 22:04:18 +0800

    use 'runTest'

--- testsuite/Main.hs
+++ testsuite/Main.hs
@@ -139,7 +139,7 @@ runClashTest = defaultMain $ clashTestRoot
         , runTest "BlackBoxFunctionHO" def{hdlTargets=[VHDL]}
         , outputTest ("tests" </> "shouldwork" </> "Signal")   allTargets [] [] "BlockRamLazy"       "main"
         -- as per group "TopEntity" VHDL tests disabled for for Scaffold
-        , runTest    ("tests" </> "shouldwork" </> "BlackBox") [Verilog] [] "Scaffold" (["","mult_prim","testBench"],"testBench",True)
+        , runTest "Scaffold" def{hdlTargets=[Verilog]}
         , runFailingTest ("tests" </> "shouldfail" </> "BlackBox") [VHDL] [] "WrongReference" (Just "Function WrongReference.myMultiply was annotated with an inline primitive for WrongReference.myMultiplyX. These names should be the same.")
         ]
       , clashTestGroup "BoxedFunctions"
